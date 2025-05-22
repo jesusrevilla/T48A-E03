60f130a658038227c8b6077fd32acf9b434de37d
GaelElias <177735@upslp.edu.mx>
2025-05-21 19:21:32 -0600

    Update code.py

--- code.py
+++ code.py
@@ -15,12 +15,9 @@ def cargar_datos():
     
 # Función para dividir y escalar los datos, test_size=0.2
 def dividir_y_escalar_datos(digits):
-  X = digits.data
-  y = digits.target
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
   scaler = StandardScaler()
-  X_train = scaler.fit_transform(X_train)
-  X_test = scaler.transform(X_test)
+  X_scaled = scaler.fit_transform(X)
+  X_train, X_test, y_train, y_test = train_test_split(X_scaled, data.target, test_size=0.2, random_state=42)
   return X_train, y_train, X_test, y_test
 
 # Función para entrenar el modelo
